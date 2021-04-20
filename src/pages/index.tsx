e0d16a62d9bb05e6c68cfedc3507e6cd86524106
JeanCDev <jeangames15@gmail.com>
2021-04-20 18:24:14 -0300

    Second day of event

---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+
+
+export default function Home(props) {
+  return (
+    <div>{JSON.stringify(props.episodes)}</div>
+  )
+}
+
+export async function getStaticProps(){
+  const response = await fetch('http://localhost:3333/episodes');
+  const data = await response.json();
+
+  console.log(data)
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60*60*8
+  }
+}
